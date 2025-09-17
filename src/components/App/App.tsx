5b4ad24f71cb1f6472ff6aa1b6f225eb5a0f42db
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-09-17 18:15:48 +0300

    final App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,37 +3,36 @@ import css from './App.module.css';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import type { Movie } from '../../types/movie';
 import { useState, useEffect } from "react";
-import { fetchMovies} from '../../services/movieService';
+import { fetchMovies, type TmdbResponse } from '../../services/movieService';
 import MovieModal from '../MovieModal/MovieModal';
-import { useQuery, keepPreviousData  } from '@tanstack/react-query';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import { Toaster } from "react-hot-toast";
 import toast from 'react-hot-toast';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import ReactPaginate from 'react-paginate';
-import type { TmdbResponse } from '../../services/movieService';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
   const [curPage, setCurPage] = useState(1);
 
-  const { data, isLoading, isError } = useQuery<TmdbResponse | null>({
+  const { data, isLoading, isError } = useQuery<TmdbResponse>({
     queryKey: ['movies', searchTerm, curPage],
     queryFn: () => fetchMovies(searchTerm, curPage),
     enabled: !!searchTerm,
-    placeholderData: keepPreviousData,  // щоб не мигало між сторінками
+    placeholderData: keepPreviousData,
   });
-
-  useEffect(() => {
-    if (isError) {
-      toast.error("There was an error, please try again...");
-    }
-    if (data && data.results.length === 0) {
-      toast.error("No movies found for your request.");
-    }
-  }, [isError, data]);
-
+   useEffect(() => {
+        if (isError) {
+            toast.error("There was an error, please try again...");
+        }
+        // Було: data.movies.length === 0
+        if (data && data.results.length === 0) {
+            toast.error("No movies found for your request.");
+        }
+   }, [isError, data]);
+  
   const handleSelectMovie = (movie: Movie) => {
     setSelectedMovie(movie);
   };
@@ -44,7 +43,7 @@ function App() {
 
   const handleSearch = (query: string) => {
     setSearchTerm(query);
-    setCurPage(1); // скидаємо на першу сторінку при новому пошуку
+    setCurPage(1);
   };
 
   const totalPages = data?.total_pages ?? 0;
@@ -53,7 +52,7 @@ function App() {
     <div>
       <Toaster />
       <SearchBar onSubmit={handleSearch} />
-  
+
       {isLoading && <Loader />}
       {isError && !isLoading && <ErrorMessage />}
 
